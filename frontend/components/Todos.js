6dae588a95433ba3ab6ca844e57a31bd66c3c728
Natalie Thompson <nataliethompson2314@gmail.com>
2024-04-10 08:23:59 -0700

    refactored using redux toolkit

--- frontend/components/Todos.js
+++ frontend/components/Todos.js
@@ -1,42 +1,43 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleShowCompletedTodos, toggleTodo } from "../state/todosSlice";
 
 const StyledTodo = styled.li`
-  text-decoration: ${pr => pr.$complete ? 'line-through' : 'initial'};
+  text-decoration: ${(pr) => (pr.$complete ? "line-through" : "initial")};
   cursor: pointer;
-`
-let id = 1
-const getNextId = () => id++
+`;
 
 export default function Todo() {
-  const todos = [ // TODO: this must come from app state!
-    { id: getNextId(), label: 'Laundry', complete: true },
-    { id: getNextId(), label: 'Groceries', complete: false },
-    { id: getNextId(), label: 'Dishes', complete: false },
-  ]
-  const showCompletedTodos = true // TODO: this must come from app state!
+  const dispatch = useDispatch();
+  const todos = useSelector((st) => st.todosState.todos);
+  const showCompletedTodos = useSelector(
+    (st) => st.todosState.showCompletedTodos
+  );
   // TODO: enable ability to complete a todo!
-  // TODO: enable toggling visibility of complete todos!
 
   return (
     <div id="todos">
       <h3>Todos</h3>
       <ul>
-        {
-          todos
-            .filter(todo => {
-              return showCompletedTodos || !todo.complete
-            })
-            .map(todo => (
-              <StyledTodo $complete={todo.complete} key={todo.id}>
-                <span>{todo.label}{todo.complete && ' ✔️'}</span>
-              </StyledTodo>
-            ))
-        }
+        {todos
+          .filter((todo) => {
+            return showCompletedTodos || !todo.complete;
+          })
+          .map((todo) => (
+            <StyledTodo 
+            onClick={() => dispatch(toggleTodo(todo.id))}
+            $complete={todo.complete} key={todo.id}>
+              <span>
+                {todo.label}
+                {todo.complete && " ✔️"}
+              </span>
+            </StyledTodo>
+          ))}
       </ul>
-      <button>
-        {showCompletedTodos ? 'Hide' : 'Show'} completed todos
+      <button onClick={() => dispatch(toggleShowCompletedTodos())}>
+        {showCompletedTodos ? "Hide" : "Show"} completed todos
       </button>
     </div>
-  )
+  );
 }
